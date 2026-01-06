1ee47f7acbd76c343e0d9e46c43d4ade38b28907
DEVESH1709 <deveshkesharwani2003@gmail.com>
2026-01-06 23:26:15 +0530

    Refactor TypeScript configuration and add storage utility functions for goals management

---
+++ src/utils/storage.ts
@@ -0,0 +1,9 @@
+import {Goal} from '../types/goal';
+
+const KEY = "syfe_goals"
+
+export const loadGoals = (): Goal[]=>
+    JSON.parse(localStorage.getItem(KEY)||"[]");
+
+export const saveGoals = (goals:Goal[])=>
+    localStorage.setItem(KEY,JSON.stringify(goals));
