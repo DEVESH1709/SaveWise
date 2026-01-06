bcdec47c3364625bb70663a6de373b55f0e722fc
DEVESH1709 <deveshkesharwani2003@gmail.com>
2026-01-06 23:36:58 +0530

    Add environment variable entries to .gitignore and implement useExchangeRate hook

---
+++ src/hooks/useExchangeRate.ts
@@ -0,0 +1,8 @@
+
+const API_KEY = import.meta.env.VITE_EXCHANGE_RATE_API_KEY;
+const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`
+
+export function useExchangeRate(){
+
+
+}
