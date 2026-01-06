42f566f18a92b47bb40c293213830f6e643a2252
DEVESH1709 <deveshkesharwani2003@gmail.com>
2026-01-06 23:29:39 +0530

    Add currency formatting utility function for USD and INR

---
+++ src/utils/currency.ts
@@ -0,0 +1,5 @@
+export const formatMoney =(amount :number,currency :"USD"|"INR")=>
+    new Intl.NumberFormat(currency ===  "USD" ? "en-US" : "en-IN",{
+        style :"currency",
+        currency
+    }) .format(amount);
